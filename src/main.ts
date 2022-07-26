e74b7bfa190c850f6f3dec678ba7048e67fd61fa
lamlam-l <lam.2002.2015@gmail.com>
2022-07-26 21:54:41 +0700

    Add tailwind

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './shared/router'
 import store from './shared/store'
+import '@/shared/assets/css/tailwind.css'
 
 createApp(App)
     .use(store)
