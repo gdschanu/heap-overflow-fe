44b2daf8ebc83c7e6b887600acfcc229e76ca73f
lamlam-l <lam.2002.2015@gmail.com>
2022-08-04 09:19:57 +0700

    ErrorHandler & Alert

--- src/App.vue
+++ src/App.vue
@@ -3,26 +3,32 @@
         <div class="content">
             <router-view />
         </div>
+        <Alert
+            :isShow="$store.state.alert.isAlert"
+            :text="$store.state.alert.message"
+            :type="$store.state.alert.type"
+            @close="$store.dispatch('closeAlert')"
+        />
     </div>
 </template>
 
 <script lang="ts" setup>
+import Alert from "./shared/components/general/Alert.vue";
 import { ref, watch } from "@vue/runtime-core";
 import { useRouter, useRoute } from "vue-router";
 import routerWatcher from "./shared/helpers/routerWatcher";
 import themeSetup from "./shared/helpers/themeSetup";
+
 // router
 const route = useRoute();
 const router = useRouter();
 
 watch(route, (to, from) => {
-    routerWatcher(to, from)
+    routerWatcher(to, from);
 });
 
 // theme
-themeSetup()
-
-
+themeSetup();
 
 
 </script>
