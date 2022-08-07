257b995f1554cbc558c27027f5d1edb347830953
Liamm999 <lelam96705@gmail.com>
2022-08-07 21:17:13 +0700

    contestDetail showing problem infor

--- src/contest/model/contest/problem.ts
+++ src/contest/model/contest/problem.ts
@@ -1,30 +1,31 @@
 import { assert } from "@vue/compiler-core";
 
-
 class Problem {
   _ordinal;
   _coreProblemId;
   _score;
-  constructor(
-    ordinal: number,
-    coreProblemId: string,
-    score: number
-  ) {
-    assert(!isNaN(ordinal))
-    this._ordinal = ordinal
+  constructor(ordinal: number, coreProblemId: string, score: number) {
+    assert(!isNaN(ordinal));
+    this._ordinal = ordinal;
 
-    assert(typeof coreProblemId === 'string')
-    this._coreProblemId = coreProblemId
+    assert(typeof coreProblemId === "string");
+    this._coreProblemId = coreProblemId;
 
-    assert(!isNaN(score))
-    this._score = score
+    assert(!isNaN(score));
+    this._score = score;
   }
 
   getOrdinal() {
     return this._ordinal;
   }
+
+  getCoreProblemId() {
+    return this._coreProblemId;
+  }
+
+  getScore() {
+    return this._score;
+  }
 }
 
-export {
-  Problem
-}
+export { Problem };
