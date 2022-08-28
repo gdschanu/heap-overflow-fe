894d54078e54079bbaba62dba8d9cfc85e2c038c
Liamm999 <lelam96705@gmail.com>
2022-08-28 11:45:58 +0700

    contest get participant

--- src/contest/model/participant/domainLogic/participant.ts
+++ src/contest/model/participant/domainLogic/participant.ts
@@ -1,5 +1,5 @@
 import { assert } from "@vue/compiler-core";
-
+import { getParticipantsAPI } from "../api/getParticipant";
 import { Participant } from "../participant";
 
 function createParticipant(contestId: string) {
@@ -7,4 +7,29 @@ function createParticipant(contestId: string) {
   return new Participant("", undefined, [], contestId);
 }
 
-export { createParticipant };
+async function getParticipants(
+  contestId: string,
+  page: number,
+  perPage: number
+): Promise<Participant[]> {
+  if (page < 0) {
+    throw new Error("page must be greater than 0");
+  }
+  if (perPage < 0) {
+    throw new Error("perPage must be greater than 0");
+  }
+
+  const participants: Participant[] = [];
+  const response = await getParticipantsAPI({ contestId, page, perPage });
+  response.data!.forEach((item) => {
+    const participant = new Participant(
+      item.coderId,
+      item.rank,
+      item.problemScores,
+      item.contestId
+    );
+    participants.push(participant);
+  });
+  return participants;
+}
+export { createParticipant, getParticipants };
